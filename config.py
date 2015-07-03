f3f1b1f709c474009b8aabeed2b7b6953bd0b230
Michael Lam <lamm@eecs.oregonstate.edu>
2015-07-03 08:58:38 -0700

    config.py accepts alternate config file path

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import json
 
-def load_settings():
+DEFAULT_CONFIG_FILE_PATH = 'config.json'
+
+def load_settings(config_file_path = DEFAULT_CONFIG_FILE_PATH):
 	"""
 	Load settings from config JSON file.
 	"""
-	with open('config.json') as f:
+	with open(config_file_path, 'r') as f:
 		config = json.load(f)
 	for i in config:
 		for j in config[i]:
