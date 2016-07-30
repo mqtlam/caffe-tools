32779fdcf2f00142c320486156530d6bb1d88ac4
Michael Lam <lamm@eecs.oregonstate.edu>
2016-07-30 10:27:46 -0700

    Improvements

--- extract_features_to_lmdb.py
+++ extract_features_to_lmdb.py
@@ -8,6 +8,7 @@ from caffetools.extract.DeepFeatures import DeepFeatures
 from caffetools.lmdb import lmdbtools
 from caffetools.lmdb.numpyserializer import NumPySerializer
 import argparse
+import os
 
 # parse arguments
 parser = argparse.ArgumentParser(description='Script to extract CNN features into LMDB')
@@ -16,16 +17,18 @@ parser.add_argument('images_path', help='path to base directory of images')
 parser.add_argument('lmdb_path', help='path to output lmdb')
 args = parser.parse_args()
 
+# check
+if not os.path.exists(args.images_path):
+	raise OSError('images_path does not exist: {0}'.format(args.images_path))
+
 # open images list
-images_list = []
 with open(args.images_list_path, 'r') as f:
-	for line in f:
-		images_list.append(line.strip())
+	images_list = [line.strip() for line in f]
 
 # loop over images
 d = DeepFeatures()
 for image in images_list:
-	path_to_image = args.images_path + image
+	path_to_image = os.path.join(args.images_path, image)
 
 	# extract features
 	features = d.extract_features(path_to_image)
