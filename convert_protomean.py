d8c2bf7bfc968391cf877f2144508ac3433a68cb
Michael Lam <lamm@eecs.oregonstate.edu>
2016-07-30 22:03:01 -0700

    Improvements

--- convert_protomean.py
+++ convert_protomean.py
@@ -1,17 +1,17 @@
 """
 Convert caffe mean file from .binaryproto to .npy
-Usage: python convert_protomean.py /path/to/proto.mean /path/to/out.npy
+Usage: python convert_protomean.py /path/to/imagenet_mean.binaryproto /path/to/output.npy
 """
 import sys
 from caffetools.mean.meantools import MeanTools
+import argparse
 
 # parse arguments
-if len(sys.argv) != 3:
-	print "Usage: python convert_protomean.py /path/to/proto.mean /path/to/out.npy"
-	sys.exit()
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+parser = argparse.ArgumentParser(description='Convert caffe mean file from .binaryproto to .npy')
+parser.add_argument('binaryproto_file', help='path to imagenet_mean.binaryproto')
+parser.add_argument('npy_file', help='path to output.npy')
+args = parser.parse_args()
 
 # convert
-MeanTools.convert_binaryproto_to_npy(input_file, output_file)
+MeanTools.convert_binaryproto_to_npy(args.binaryproto_file, args.npy_file)
 
